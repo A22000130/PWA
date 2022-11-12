0833b9d93361b12cf68ce502aefe4ef1565a5b8d
Muhammad Fairuz Yorisyahputra <91519652+A22000130@users.noreply.github.com>
2022-11-12 16:25:50 +0700

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,13 +1,20 @@
-document.addEventListener("DOMContentLoaded", init, false);
-function init() {
-  if ("serviceWorker" in navigator && navigator.onLine) {
-    navigator.serviceWorker.register("/service-worker.js").then(
-      (reg) => {
-        console.log("Registrasi service worker Berhasil", reg);
-      },
-      (err) => {
-        console.error("Registrasi service worker Gagal", err);
-      }
-    );
-  }
-}
+if (navigator.serviceWorker) {
+      navigator.serviceWorker.register(
+        '/pwa-rental-mobil-sederhana/service-worker.js',
+        { scope: '/pwa-rental-mobil-sederhana/' }
+      )
+    }
+
+// document.addEventListener("DOMContentLoaded", init, false);
+// function init() {
+//   if ("serviceWorker" in navigator && navigator.onLine) {
+//     navigator.serviceWorker.register("/service-worker.js").then(
+//       (reg) => {
+//         console.log("Registrasi service worker Berhasil", reg);
+//       },
+//       (err) => {
+//         console.error("Registrasi service worker Gagal", err);
+//       }
+//     );
+//   }
+// }
