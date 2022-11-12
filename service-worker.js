b1b15302bc560249970912615f61aac1891ea926
A22000130 <a2.2000130@mhs.stmik-sumedang.ac.id>
2022-11-12 13:37:07 +0700

    commit kelima

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
-var GHPATH = '/PWA---Rental-Mobil-Sederhana';
-var APP_PREFIX = 'rmspwa_';
-var VERSION = 'version_00';
+// var GHPATH = '/PWA---Rental-Mobil-Sederhana';
+// var APP_PREFIX = 'rmspwa_';
+// var VERSION = 'version_00';
 
-var URLS = [
-  `${GHPATH}/`,
-  `${GHPATH}/index.html`,
-  `${GHPATH}/js/rentalMobil.js`
-]
+// var URLS = [
+//   `${GHPATH}/`,
+//   `${GHPATH}/index.html`,
+//   `${GHPATH}/js/rentalMobil.js`
+// ]
 
 const CACHE_NAME = 'SW-001';
 const toCache = [
